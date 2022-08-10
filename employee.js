91a5f981c6a51cff063596c4b56f662a826190e2
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-08-09 18:34:29 -0700

    employee bonus

--- employee.js
+++ employee.js
@@ -1,5 +1,5 @@
 class Employee {
-  constructor(name, title, salary, manager = null) {
+  constructor(name, salary, title, manager = null) {
     this.name = name;
     this.title = title;
     this.salary = salary;
@@ -8,6 +8,22 @@ class Employee {
       manager.addEmployee(this);
     }
   }
+  calculateBonus(multiplier) {
+    return this.salary * multiplier;
+  }
+  /*
+Inside of `_totalSubSalary()`, create a sum variable. Check to see if each
+`employee` is an instance of `Manager`. If so, add their `salary` plus a
+recursive call to their `_totalSubSalary()` to sum. If not, simply add
+their `salary` to sum. 
+
+The `calculateBonus` logic should look something like this:
+
+```plaintext
+bonus = (manager's salary + total salary of all employees under them)
+ * multiplier
+```
+*/
 }
 
 module.exports = Employee;
