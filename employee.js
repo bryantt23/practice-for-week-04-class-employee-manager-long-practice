5ab2c9e4882c186e0001e94bfe2c9abd6731f6aa
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-08-09 19:30:42 -0700

    made the mistake of making manager null a second time

--- employee.js
+++ employee.js
@@ -4,26 +4,13 @@ class Employee {
     this.title = title;
     this.salary = salary;
     this.manager = manager;
-    if (manager !== null) {
+    if (manager) {
       manager.addEmployee(this);
     }
   }
   calculateBonus(multiplier) {
     return this.salary * multiplier;
   }
-  /*
-Inside of `_totalSubSalary()`, create a sum variable. Check to see if each
-`employee` is an instance of `Manager`. If so, add their `salary` plus a
-recursive call to their `_totalSubSalary()` to sum. If not, simply add
-their `salary` to sum. 
-
-The `calculateBonus` logic should look something like this:
-
-```plaintext
-bonus = (manager's salary + total salary of all employees under them)
- * multiplier
-```
-*/
 }
 
 module.exports = Employee;
--- manager.js
+++ manager.js
@@ -1,49 +1,29 @@
 const Employee = require('./employee');
 
 class Manager extends Employee {
-  constructor(name, title, salary, manager = null, employees = []) {
-    super(name, title, salary, (manager = null));
-    this.employees = employees;
+  constructor(name, salary, title, manager = null) {
+    super(name, salary, title, manager);
+    this.employees = [];
   }
   addEmployee(employee) {
     this.employees.push(employee);
   }
   calculateBonus(multiplier) {
-    const sum = this._totalSubSalary(this.employees) + this.salary;
-    console.log(
-      '🚀 ~ file: manager.js ~ line 17 ~ Manager ~ calculateBonus ~ sum',
-      sum,
-      multiplier
-    );
-
-    return Number(sum) * multiplier;
+    return this._totalSubSalary(this) * multiplier;
   }
-  _totalSubSalary(employees) {
-    if (!employees) return 0;
-    let res = 0;
-    for (const employee of employees) {
-      if (employee.manager == null) {
-        //is manager
-        res += employee.salary + this._totalSubSalary(employee.employees);
-      } else {
-        res += employee.salary;
-      }
+  _totalSubSalary(employee) {
+    //base case
+    if (!employee.employees) {
+      console.log(employee.salary);
+      return employee.salary;
+    }
+
+    let res = employee.salary;
+    for (const emp of employee.employees) {
+      res += this._totalSubSalary(emp);
     }
     return res;
   }
-  /*
-Inside of `_totalSubSalary()`, create a sum variable. Check to see if each
-`employee` is an instance of `Manager`. If so, add their `salary` plus a
-recursive call to their `_totalSubSalary()` to sum. If not, simply add
-their `salary` to sum. 
-
-The `calculateBonus` logic should look something like this:
-
-```plaintext
-bonus = (manager's salary + total salary of all employees under them)
- * multiplier
-```
-*/
 }
 
 module.exports = Manager;
